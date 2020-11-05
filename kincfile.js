3b83d7b82fed5f4a6becfa7d8fe82e8a72a91ed3
Apprentice Alchemist <azzer159@yahoo.com>
2020-11-05 16:11:44 +0100

    Try to fix the workflow

--- kincfile.js
+++ kincfile.js
@@ -2,6 +2,17 @@ let project = new Project("KincHL");
 project.setDebugDir("Deployment");
 project.addFile("src/**");
 project.addIncludeDir("src/");
-project.addIncludeDir(require("process").env["HASHLINK"] + "/include");
-project.addLib(require("process").env["HASHLINK"] + "/libhl");
+
+if(require("process").env["GITHUB_WORKSPACE"] != null){
+    project.addIncludeDir(require("process").env["HASHLINK_INCLUDE"]);
+    project.addLib(require("process").env["HASHLINK_BIN"] + "/libhl");
+}else if(require("process").env["HASHLINK"]){
+    const hashlink = require("process").env["HASHLINK"];
+    project.addIncludeDir(hashlink + "/include");
+    project.addLib(hashlink + "/libhl");
+}else{
+    console.log("%HASHLINK% not defined, exiting.");
+    require("process").exit(1);
+}
+
 resolve(project);
