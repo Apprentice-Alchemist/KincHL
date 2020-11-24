f6cac12f662c63903016330b04542f89816e6956
Apprentice Alchemist <azzer159@yahoo.com>
2020-11-24 16:02:02 +0100

    ........

--- src/callbacks.c
+++ src/callbacks.c
@@ -4,19 +4,19 @@
 
 
 #define MAKE_CALLBACK(name,expr,hl_type)\
-  static vclosure* ##name##_cb = NULL;\
+  static vclosure* name##_cb = NULL;\
   static expr\
-  HL_PRIM void HL_NAME(hl_set_##name##_callback)(vclosure *cb){\
-    if( ##name##_cb != NULL){\
-      hl_remove_root(##name##_cb);\
+  HL_PRIM void HL_NAME(hl_set_ ##name## _callback)(vclosure *cb){\
+    if(name## _cb != NULL){\
+      hl_remove_root(name## _cb);\
     }\
-    ##name##_cb = cb;\
+    name## _cb = cb;\
     if(cb != NULL){\
-      hl_add_root(##name##_cb);\
-      kinc_set_##name##_callback(internal_##name##_callback);\
+      hl_add_root(name## _cb);\
+      kinc_set_ ##name## _callback(internal_ ##name## _callback);\
     }\
   }\
-  DEFINE_PRIM(_VOID,hl_set_##name##_callback,hl_type)
+  DEFINE_PRIM(_VOID,hl_set_ ##name## _callback,hl_type)
 
 MAKE_CALLBACK(update, void internal_update_callback() {
   bool isexc = false;
@@ -32,155 +32,155 @@ MAKE_CALLBACK(update, void internal_update_callback() {
   }
 }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(foreground, void internal_foreground_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(foreground_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in foreground callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(foreground, void internal_foreground_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(foreground_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in foreground callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(resume, void internal_resume_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(resume_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in resume callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(resume, void internal_resume_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(resume_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in resume callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(pause, void internal_pause_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(pause_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in pause callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(pause, void internal_pause_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(pause_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in pause callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(background, void internal_background_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(background_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in background callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(background, void internal_background_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(background_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in background callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(shutdown, void internal_shutdown_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(shutdown_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in shutdown callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(shutdown, void internal_shutdown_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(shutdown_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in shutdown callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(drop_files, void internal_drop_files_callback(wchar_t* s) {
-  vdynamic* arg1 = hl_alloc_strbytes(s);
-  vdynamic* args[1] = { arg1 };
-  bool isexc = false;
-  hl_dyn_call_safe(drop_files_cb, args, 1, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in drop files callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _STRING))
+// MAKE_CALLBACK(drop_files, void internal_drop_files_callback(wchar_t* s) {
+//   vdynamic* arg1 = hl_alloc_strbytes(s);
+//   vdynamic* args[1] = { arg1 };
+//   bool isexc = false;
+//   hl_dyn_call_safe(drop_files_cb, args, 1, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in drop files callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _STRING))
 
-MAKE_CALLBACK(cut, char* internal_cut_callback() {
-  bool isexc = false;
-  char* ret = hl_to_utf8(((vstring*)hl_dyn_call_safe(cut_cb, NULL, 0, &isexc))->bytes);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in cut callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-    return NULL;
-  }
-  return ret;
-}, _FUN(_STRING, _NO_ARG))
+// MAKE_CALLBACK(cut, char* internal_cut_callback() {
+//   bool isexc = false;
+//   char* ret = hl_to_utf8(((vstring*)hl_dyn_call_safe(cut_cb, NULL, 0, &isexc))->bytes);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in cut callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//     return NULL;
+//   }
+//   return ret;
+// }, _FUN(_STRING, _NO_ARG))
 
-MAKE_CALLBACK(copy, char* internal_copy_callback() {
-  bool isexc = false;
-  char* ret = hl_to_utf8(((vstring*)hl_dyn_call_safe(copy_cb, NULL, 0, &isexc))->bytes);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in copy callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-    return NULL;
-  }
-  return ret;
-}, _FUN(_STRING, _NO_ARG))
+// MAKE_CALLBACK(copy, char* internal_copy_callback() {
+//   bool isexc = false;
+//   char* ret = hl_to_utf8(((vstring*)hl_dyn_call_safe(copy_cb, NULL, 0, &isexc))->bytes);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in copy callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//     return NULL;
+//   }
+//   return ret;
+// }, _FUN(_STRING, _NO_ARG))
 
-MAKE_CALLBACK(paste, void internal_paste_callback(char* s) {
-  vdynamic* arg1 = hl_alloc_strbytes(hl_to_utf16(s));
-  vdynamic* args[1] = { arg1 };
-  bool isexc = false;
-  hl_dyn_call_safe(paste_cb, args, 1, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in paste callback");
-    varray* stack = hl_exception_stack();
-    // hl_dump_stack();
-    if (stack != NULL) {
-      for (int i = 0; i < stack->size; i++) {
-        kinc_log(KINC_LOG_LEVEL_WARNING, hl_to_utf8(hl_aptr(stack, uchar*)[i]));
-      }
-    }
-  }
-}, _FUN(_VOID, _STRING))
+// MAKE_CALLBACK(paste, void internal_paste_callback(char* s) {
+//   vdynamic* arg1 = hl_alloc_strbytes(hl_to_utf16(s));
+//   vdynamic* args[1] = { arg1 };
+//   bool isexc = false;
+//   hl_dyn_call_safe(paste_cb, args, 1, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in paste callback");
+//     varray* stack = hl_exception_stack();
+//     // hl_dump_stack();
+//     if (stack != NULL) {
+//       for (int i = 0; i < stack->size; i++) {
+//         kinc_log(KINC_LOG_LEVEL_WARNING, hl_to_utf8(hl_aptr(stack, uchar*)[i]));
+//       }
+//     }
+//   }
+// }, _FUN(_VOID, _STRING))
 
-MAKE_CALLBACK(login, void internal_login_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(login_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in login callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(login, void internal_login_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(login_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in login callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
 
-MAKE_CALLBACK(logout, void internal_logout_callback() {
-  bool isexc = false;
-  hl_dyn_call_safe(login_cb, NULL, 0, &isexc);
-  if (isexc) {
-    kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in login callback");
-    varray* stack = hl_exception_stack();
-    for (int i = 0; i < stack->size; i++) {
-      kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
-      kinc_log(KINC_LOG_LEVEL_INFO, "\n");
-    }
-  }
-}, _FUN(_VOID, _NO_ARG))
+// MAKE_CALLBACK(logout, void internal_logout_callback() {
+//   bool isexc = false;
+//   hl_dyn_call_safe(login_cb, NULL, 0, &isexc);
+//   if (isexc) {
+//     kinc_log(KINC_LOG_LEVEL_WARNING, "Exception occured in login callback");
+//     varray* stack = hl_exception_stack();
+//     for (int i = 0; i < stack->size; i++) {
+//       kinc_log(KINC_LOG_LEVEL_INFO, hl_to_utf8(hl_to_string(hl_aptr(stack, vdynamic*)[i])));
+//       kinc_log(KINC_LOG_LEVEL_INFO, "\n");
+//     }
+//   }
+// }, _FUN(_VOID, _NO_ARG))
--- src/kinchl.h
+++ src/kinchl.h
@@ -14,19 +14,19 @@ void EMPTY_INIT(void* obj);
 void EMPTY_DESTROY(void* obj);
 
 #define ALLOC_OBJ(type,name, hl_type,init,destroy)\
-  HL_PRIM type *HL_NAME(##name##_hl_alloc)(){\
+  HL_PRIM type *HL_NAME( name## _hl_alloc)(){\
     type * o = (type*)hl_gc_alloc_raw(sizeof(type));\
     return o;\
   }\
-  HL_PRIM void HL_NAME(##name##_hl_init)(type * o){\
+  HL_PRIM void HL_NAME( name## _hl_init)(type * o){\
     init(o);\
   }\
-  HL_PRIM void HL_NAME(##name##_hl_destroy)(type * o){\
+  HL_PRIM void HL_NAME( name## _hl_destroy)(type * o){\
     destroy(o);\
   }\
-  DEFINE_PRIM(hl_type,##name##_hl_alloc,_NO_ARG)\
-  DEFINE_PRIM(_VOID,##name##_hl_init,hl_type)\
-  DEFINE_PRIM(_VOID,##name##_hl_destroy,hl_type)
+  DEFINE_PRIM(hl_type, name## _hl_alloc,_NO_ARG)\
+  DEFINE_PRIM(_VOID, name## _hl_init,hl_type)\
+  DEFINE_PRIM(_VOID, name## _hl_destroy,hl_type)
 
 #define MAKE_GET_SET(obj_type, name,field_name, type, hl_obj, hl_ret)                        \
   HL_PRIM type HL_NAME(name##_hl_get_##field_name)(obj_type * o) {           \
