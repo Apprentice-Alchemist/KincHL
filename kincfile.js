1b61de6034a48de958bb27406c79f0396ecc5405
Apprentice Alchemist <azzer159@yahoo.com>
2020-11-24 17:00:47 +0100

    Fix kincfile.js

--- kincfile.js
+++ kincfile.js
@@ -5,11 +5,11 @@ project.addIncludeDir("src/");
 
 if(require("process").env["GITHUB_WORKSPACE"]){
     project.addIncludeDir(require("process").env["GITHUB_WORKSPACE"] + "/hashlink/src");
-    project.addLib(require("process").platform === "linux" ? "hl" : "libhl");
+    project.addLib((require("process").platform === "linux" || require("process").platform === "darwin") ? "hl" : "libhl");
 }else if(require("process").env["HASHLINK"]){
     const hashlink = require("process").env["HASHLINK"];
     project.addIncludeDir(hashlink + "/include");
-    project.addLib(require("process").platform === "linux" ? "hl" : "libhl");
+    project.addLib((require("process").platform === "linux" || require("process").platform === "darwin") ? "hl" : "libhl");
 }else{
     console.log("%HASHLINK% not defined, exiting.");
     require("process").exit(1);
