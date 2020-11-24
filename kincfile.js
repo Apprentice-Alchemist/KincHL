6286dad192959cec11410d8791566c9d028213a1
Apprentice Alchemist <azzer159@yahoo.com>
2020-11-24 16:37:04 +0100

    Just work already...

--- kincfile.js
+++ kincfile.js
@@ -5,11 +5,11 @@ project.addIncludeDir("src/");
 
 if(require("process").env["GITHUB_WORKSPACE"]){
     project.addIncludeDir(require("process").env["GITHUB_WORKSPACE"] + "/hashlink/src");
-    project.addLib("libhl");
+    project.addLib(require("process").platform = "linux" ? "hl" : "libhl");
 }else if(require("process").env["HASHLINK"]){
     const hashlink = require("process").env["HASHLINK"];
     project.addIncludeDir(hashlink + "/include");
-    project.addLib("libhl");
+    project.addLib(require("process").platform = "linux" ? "hl" : "libhl");
 }else{
     console.log("%HASHLINK% not defined, exiting.");
     require("process").exit(1);
