b4698793045a37648881d388eb7aa07d0649f36c
Apprentice Alchemist <azzer159@yahoo.com>
2021-01-31 15:57:21 +0100

    [skip ci] remove logging stuff

--- kincfile.js
+++ kincfile.js
@@ -12,8 +12,6 @@ switch(platform){
         break;
     case Platform.OSX:
         if (process.env["GITHUB_WORKSPACE"]) {
-            // project.addLib(process.env["GITHUB_WORKSPACE"] + "/hashlink/libhl.dylib");
-            // project.addIncludeDir(process.env["GITHUB_WORKSPACE"] + "/hashlink/src");
             project.addIncludeDir("/usr/local/include");
             project.addLib("/usr/local/lib/libhl.dylib");
         }else{
@@ -22,15 +20,6 @@ switch(platform){
         break;
     case Platform.Windows:
         if (process.env["GITHUB_WORKSPACE"]) {
-            console.log(process.env["GITHUB_WORKSPACE"]);
-            console.log(process.env["GITHUB_WORKSPACE"] + "/hashlink/x64/Release/libhl");
-            let path = process.env["GITHUB_WORKSPACE"] + "/hashlink/x64/Release/libhl";
-            try{
-            console.log(fs.existsSync(path));
-            console.log(fs.readdirSync(process.env["GITHUB_WORKSPACE"] + "/hashlink"));
-            console.log(fs.readdirSync(process.env["GITHUB_WORKSPACE"] + "/hashlink/x64"));
-            console.log(fs.readdirSync(process.env["GITHUB_WORKSPACE"] + "/hashlink/x64/Release"));
-            }catch(e){}
             project.addLib(process.env["GITHUB_WORKSPACE"] + "/hashlink/x64/Release/libhl");
             project.addIncludeDir(process.env["GITHUB_WORKSPACE"] + "/hashlink/src");
         } else {
