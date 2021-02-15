a7a07a07171a61e59f3e1d78eed105f6280b474b
Apprentice Alchemist <azzer159@yahoo.com>
2021-02-15 17:23:56 +0100

    Make the build script error if something goes wrong

--- .github/Build.hx
+++ .github/Build.hx
@@ -1,5 +1,11 @@
 import haxe.io.Path;
 
+inline function assert(b:Bool, err = "assert")
+	if (!b) {
+		Sys.println(err);
+		Sys.exit(1);
+	}
+
 function main() {
 	final args = Sys.args();
 	final g_api = args[0];
@@ -11,7 +17,9 @@ function main() {
 	final n_args = ["Kinc/make.js", "--dynlib", "--noshaders", "-g", g_api];
 	if (sys_name != "mac")
 		n_args.push("--compile");
-	Sys.command("node", n_args);
-	if (sys_name == "mac")
-		Sys.command("xcodebuild", ["-configuration", "Release", "-project", "KincHL.xcodeproj", "ARCHS=x86_64"]);
+	assert(Sys.command("node", n_args) == 0, "kincmake error");
+	if (sys_name == "mac") {
+		Sys.setCwd("build");
+		assert(Sys.command("xcodebuild", ["-configuration", "Release", "-project", "KincHL.xcodeproj", "ARCHS=x86_64"]) == 0, "xcodebuild error");
+	}
 }
