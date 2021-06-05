5561191c0720627f05c3ce4d18c2a6a686139ede
Apprentice Alchemist <azzer159@yahoo.com>
2021-06-05 16:42:01 +0200

    Update Build.hx

--- .github/Build.hx
+++ .github/Build.hx
@@ -20,12 +20,11 @@ function main() {
 		Sys.putEnv("HASHLINK_BIN", Path.normalize(gw + "/hashlink/x64/Release"));
 	}
 	final n_args = ["Kinc/make.js", "--dynlib", "--noshaders", "-g", g_api];
-	if(Sys.command("node", n_args) != 0)
+	if (Sys.command("node", n_args) != 0)
 		Sys.exit(1);
 	Sys.setCwd("build");
 	FileSystem.createDirectory("bin");
 	if (sys_name == "mac") {
-		// File.saveContent("KincHL.xcodeproj",File.getContent("KincHL.xcodeproj").replace("KincHL.dylib","kinc.hdll".replace("KincHL","kinc")));
 		if (Sys.command("xcodebuild", [
 			"-configuration",
 			"Release",
@@ -33,9 +32,9 @@ function main() {
 			"KincHL.xcodeproj",
 			"ARCHS=x86_64",
 			"EXECUTABLE_PATH=kinc.hdll",
-			"EXECUTABLE_FOLDER_PATH=bin"]) != 0)
+			"EXECUTABLE_FOLDER_PATH=bin"
+		]) != 0)
 			Sys.exit(1);
-		// File.copy("build/Release/kinc.hdll","bin/kinc.hdll");
 	} else if (sys_name == "windows") {
 		if (Sys.command("MSBuild", [
 			"KincHL.vcxproj",
@@ -43,8 +42,6 @@ function main() {
 			"/p:Configuration=Release,Platform=x64,OutDir=bin/,TargetExt=.hdll,TargetName=kinc"
 		]) != 0)
 			Sys.exit(1);
-		File.copy("Release/kinc.hdll", "bin/kinc.hdll");
-		File.copy("Release/kinc.lib", "bin/kinc.lib");
 	} else if (sys_name == "linux") {
 		File.saveContent("Release/makefile", File.getContent("Release/makefile").replace('-o "KincHL.so"', '-o "kinc.hdll"'));
 		Sys.setCwd("Release");
@@ -53,9 +50,9 @@ function main() {
 		Sys.setCwd("..");
 		File.copy("Release/kinc.hdll", "bin/kinc.hdll");
 	}
-	// try
-		File.copy("bin/kinc.hdll", Path.normalize(gw + "/" + "kinc.hdll"))
-	// catch (_) {};
+
+	File.copy("bin/kinc.hdll", Path.normalize(gw + "/" + "kinc.hdll"));
+
 	try
 		File.copy("bin/kinc.lib", Path.normalize(gw + "/" + "kinc.lib"))
 	catch (_) {};
